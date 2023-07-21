191078d18ad56f0ff08994fdfd75e841638fcb55
lk23286 <lk23286@gmail.com>
2023-07-21 17:22:44 +0200

    Initial Commit

---
+++ LGL-Calculator/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LGL-Calculator
+//
+//  Created by Laszlo Kovacs on 2023. 07. 21..
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
